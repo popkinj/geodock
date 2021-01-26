d54b51d1e2e443b1bd393975a95b30d5fdf8e7d1
Jamie Popkin <popkinj@littleearth.ca>
2021-01-26 12:48:42 -0800

    Adding a new default workspace

--- config.sh
+++ config.sh
@@ -2,4 +2,19 @@
 curl -X PUT \
   --header 'Content-Type: application/json' \
   -d "{\"newPassword\": \"$ADMIN_PASS\"}" \
-  http://admin:geoserver@localhost:8080/geoserver/rest/security/self/password
+  -L "http://admin:geoserver@localhost:8080/geoserver/rest/security/self/password"
+
+# Geoserver needs a bit to register the new password
+sleep 5
+
+# Create a new workspace called invasives
+curl -X POST \
+  --header 'Content-Type: application/json' \
+  -d "{\"workspace\":{ \"name\":\"invasives\" }}" \
+  -L "http://admin:$ADMIN_PASS@localhost:8080/geoserver/rest/workspaces"
+
+# Make invasives the default workspace
+curl -X PUT \
+  --header 'Content-Type: application/json' \
+  -d "{\"workspace\":{ \"name\":\"invasives\" }}" \
+  -L "http://admin:$ADMIN_PASS@localhost:8080/geoserver/rest/workspaces/default"
